d93a31bc0590ea771ec7dada559e413b81be66c3
Elvanos <xfenix@seznam.cz>
2021-07-09 18:38:42 +0200

    First new FA commit

---
+++ src/boot/i18n.ts
@@ -0,0 +1,16 @@
+import { boot } from 'quasar/wrappers'
+import { createI18n } from 'vue-i18n'
+
+import messages from 'src/i18n'
+
+const i18n = createI18n({
+  locale: 'en-US',
+  messages
+})
+
+export default boot(({ app }) => {
+  // Set i18n instance on app
+  app.use(i18n)
+})
+
+export { i18n }
